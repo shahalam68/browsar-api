ac6cf0520565a02b283e878f870e069d6e845d6a
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-06-15 17:20:00 +0600

    first commit

---
+++ cookis.js
@@ -0,0 +1,9 @@
+const getCookisByName = name =>{
+    const cookieString = document.cookie;
+    const cookieParts = cookieString.split('; ');
+    const cookie = cookieParts.find(c => c.includes(name));
+    if(cookie){
+        const cookieValue = cookie.split('=')[1]
+        return cookieValue;
+    }
+}
